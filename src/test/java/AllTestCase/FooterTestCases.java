8dfd1730b38ace18838aa70aa67b464fcf9a18eb
priyanka <priyankabundelel@gmail.com>
2023-01-23 15:45:59 +0530

    Added1

--- src/test/java/AllTestCase/FooterTestCases.java
+++ src/test/java/AllTestCase/FooterTestCases.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -14,20 +15,44 @@ import PageObjectModel1.FooterPageObject;
 public class FooterTestCases extends BaseClass {
 	@Test
 	public void FooterPage() throws IOException {
-
-
+		
+		
 		FooterPageObject vip = new FooterPageObject(driver);
-		vip.AboutUs().click();      //Information
+		vip.AboutUs().click(); // Information
 		vip.DeliveryInfo1().click();
 		vip.PrivacyPagefooter().click();
 		vip.Termandcondi().click();
-		
-		
-		vip.ReturnsClick1().click();                   //Customer Service
+
+		vip.ReturnsClick1().click(); // Customer Service
 		vip.ReturnsTeleph().sendKeys(Constants.Telephone);
 		vip.orderId().sendKeys(Constants.MentionORderid);
 		vip.SelectCalender().click();
-		vip.Cliyesforreturn().click();
+
+		
+		  String month=""; 
+		  
+		  while(!(month.equals(Constants.MonthandYear))) {
+		  vip.NextMonthclick1().click(); 
+		  
+		  month=vip.Monthandyear().getText(); 
+		  }
+		  vip.datesSelect();
+		
+		  try {
+		  for(WebElement k:vip.datesSelect()) {
+			  if(k.getText().equals("1"));
+			  k.click();
+			  break;
+			  
+		  }
+		
+		  }
+		  catch(StaleElementReferenceException e) {
+			System.out.println("stale element exception");
+		 }
+		 
+
+	    vip.Cliyesforreturn().click();
 
 		List<WebElement> a = driver.findElements(By.xpath("//input[@type='radio']"));
 		for (int i = 0; i < a.size(); i++) {
@@ -41,9 +66,7 @@ public class FooterTestCases extends BaseClass {
 		vip.Clickon_Brands().click();
 		vip.Click_on_Gift_Sertificate().click();
 		vip.Click_on_Afiliate().click();
-		
-		
-			}
-	
+
+	}
 
 }
